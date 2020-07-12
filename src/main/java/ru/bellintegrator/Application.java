7282d23f9779f907efeedce781501a3aa4143778
Nikolay Marchenko <a4241338a@yandex.ru>
2020-07-12 21:33:36 +0300

    "Добавил Application.java"

---
+++ src/main/java/ru/bellintegrator/Application.java
@@ -0,0 +1,36 @@
+package ru.bellintegrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Locale;
+
+@EnableSwagger2
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public TaskExecutor controllerPool() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setMaxPoolSize(Runtime.getRuntime().availableProcessors() + 1);
+        executor.setQueueCapacity(25);
+        return executor;
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
+        sessionLocaleResolver.setDefaultLocale(Locale.US);
+        return sessionLocaleResolver;
+    }
+}
