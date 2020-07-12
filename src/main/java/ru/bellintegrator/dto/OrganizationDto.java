0b32b25266dddbdc37ec674b870c3438eb2479f2
Nikolay Marchenko <a4241338a@yandex.ru>
2020-07-12 21:27:52 +0300

    "Добавил OrganizationDto"

---
+++ src/main/java/ru/bellintegrator/dto/OrganizationDto.java
@@ -0,0 +1,50 @@
+package ru.bellintegrator.dto;
+
+import ru.bellintegrator.model.organization.Organization;
+
+public class OrganizationDto {
+    public String name;
+    public String fullName;
+    public String inn;
+    public String kpp;
+    public String address;
+    public String phone;
+    public Boolean isActive;
+
+
+
+    public OrganizationDto() {
+    }
+
+    public OrganizationDto(String name, String fullName, String inn, String kpp, String address, String phone, Boolean isActive) {
+        this.name = name;
+        this.fullName = fullName;
+        this.inn = inn;
+        this.kpp = kpp;
+        this.address = address;
+        this.phone = phone;
+        this.isActive = isActive;
+    }
+
+    public Organization fromDto(){
+        return new Organization(name, fullName, inn, kpp, address, phone, isActive);
+    }
+
+    public static OrganizationDto toDto(Organization organization){
+        return new OrganizationDto(organization.getName(), organization.getFullName(), organization.getInn(), organization.getKpp(),
+                organization.getAddress(), organization.getPhone(), organization.getIsActive());
+    }
+
+    @Override
+    public String toString() {
+        return "OrganizationDto{" +
+                "name='" + name + '\'' +
+                ", fullName='" + fullName + '\'' +
+                ", inn='" + inn + '\'' +
+                ", kpp='" + kpp + '\'' +
+                ", address='" + address + '\'' +
+                ", phone='" + phone + '\'' +
+                ", isActive=" + isActive +
+                '}';
+    }
+}
