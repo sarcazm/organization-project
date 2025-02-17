da4cd9c0b2e157d3cb227a6b3375c94ffa49d147
Nikolay <Nikol.Marchenko@x5.ru>
2025-02-17 09:37:09 +0300

    commit method

--- src/main/java/ru/bellintegrator/controller/office/OfficeController.java
+++ src/main/java/ru/bellintegrator/controller/office/OfficeController.java
@@ -6,4 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/api/office")
 public class OfficeController {
+    void someMethod(){
+
+    }
 }
